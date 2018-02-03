f5ee4c69588bd93f0452a721b2bc47dd51adfe7c
Gustavo Leon <gleondiaz@gmail.com>
2018-02-02 23:23:14 -0300

    Some fixes on the bash script

--- folder-structureGodot.sh
+++ folder-structureGodot.sh
@@ -1,9 +1,9 @@
 mkdir -p {raw_assets,builds,src}
-mkdir -p src/{sfx,music,game,ui,menus}
+
 touch {.gitignore,README.md}
 
 echo "# Godot-specific ignores
-.import/
+**.import/
 export.cfg
 export_presets.cfg" >> .gitignore
 
@@ -13,12 +13,14 @@ echo "
 
 echo "
 # System/tool-specific ignores
-.directory
+**.directory
 *~" >> .gitignore
 
 echo "# A Game made with GODOT Game Engine
 yes a game made with godot" >> README.md
 
+read -p "Now open Godot and create New Project under src as the root directory then press enter..."
+mkdir -p src/{sfx,music,game,ui,menus}
 
 git init
 
